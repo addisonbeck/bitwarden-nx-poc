7835f797b328e854f4a90071170ee50be9de418f
addisonbeck <github@addisonbeck.com>
2025-04-23 18:09:49 -0400

    commit 10: convert cli to nx

--- apps/cli/webpack.config.js
+++ apps/cli/webpack.config.js
@@ -2,30 +2,42 @@ const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  entry: './src/index.ts',
-  target: 'node',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+module.exports = (config, context) => { 
+  const absoluteOutputPath = path.resolve(context.context.root, context.options.outputPath);
+
+  return {
+    entry: 'apps/cli/src/index.ts',
+    target: 'node',
+    mode: context.configurationName === 'production' ? 'production' : 'development', 
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            configFile: 'apps/cli/tsconfig.json' 
+          }
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      plugins: [new TsconfigPathsPlugin({ configFile: 'tsconfig.base.json' })],
+    },
+    output: {
+      path: absoluteOutputPath, 
+      filename: 'main.js'      
+    },
+    externals: [
+      nodeExternals({
+        modulesDir: "../../node_modules",
+        allowlist: [/@bitwarden/],
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin({ configFile: '../../tsconfig.base.json' })], 
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'build'),
-  },
-  externals: [nodeExternals()],
-  node: {
-    __dirname: false,
-    __filename: false,
-  }
+    node: {
+      __dirname: false,
+      __filename: false,
+    }
+  };
 };
